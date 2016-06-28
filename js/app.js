ecb218571faf7a1e0e6aeb6b1e4970d82a16517a
sawheeldon <simon@jampot.design>
2016-06-28 23:38:16 +0100

    p3 connecting wiki page

--- js/app.js
+++ js/app.js
@@ -1,28 +1,49 @@
-$(document).ready(function () {
+//function to take team name and go to wikipedia
+
+var wikiSearch = function (data) {
+    var searchTeam = $("#team-name").val();
+    var dynamicURL = "https://en.wikipedia.org/w/api.php?action=parse&page=" + searchTeam + " Football Club " + "&format=json&callback=?";
     $.ajax({
-        type: "GET",
-        url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?",
-        contentType: "application/json; charset=utf-8",
-        async: false,
-        dataType: "json",
-        success: function (data, textStatus, jqXHR) {
-
-            var markup = data.parse.text["*"];
-            var blurb = $('<div></div>').html(markup);
-
-            // remove links as they will not work
-            blurb.find('a').each(function () {
-                $(this).replaceWith($(this).html());
+            url: dynamicURL,
+            type: 'GET',
+            contentType: "application/json; charset=utf-8",
+            async: false,
+            dataType: 'jsonp',
+            success: function (data, text, jqXHR) {
+                console.log(data);
+            },
+        })
+        .done(function showResult(data) {
+            //console.log("success");
+            var html = "";
+            $.each(data, function (index, value) {
+                html += '<p>' + value.displaytitle + '</p>'
+                console.log(value.displaytitle);
             });
+            $('#wikiResult').html(html);
+        })
+        .fail(function (jqXHR, error) { //this waits for the ajax to return with an error promise object
+            var errorElem = showError(error);
+            $('#wikiResult').append(errorElem);
+        })
+        .always(function () {
+            console.log("complete");
+        });
+};
 
-            // remove any references
-            blurb.find('sup').remove();
+//function to show wiki team information
 
-            // remove cite error
-            blurb.find('.mw-ext-cite-error').remove();
-            $('#test-result').html($(blurb).find('p'));
 
-        },
-        error: function (errorMessage) {}
+//function to go and get team name and find fixtures
+
+//function to hide all the tabs
+
+//function to show all the tabs when search is complete
+
+$(function () {
+
+    $('#submit').on("click", function (e) {
+        e.preventDefault();
+        wikiSearch();
     });
 });
